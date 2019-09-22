58b7a45fce69ab6dd3e2b14d33aa42cc84386dfe
Carl Loa Odin <loa@nonetype.io>
2019-09-22 19:13:32 +0200

    Adjust homing travel speeds

--- MarlinConfiguration.h
+++ MarlinConfiguration.h
@@ -781,7 +781,7 @@
 #define MIN_PROBE_EDGE 10
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 8000
+#define XY_PROBE_SPEED 50
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
@@ -1147,8 +1147,8 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_XY (10*50)
+#define HOMING_FEEDRATE_Z  (10*50)
 
 // @section calibrate
 
